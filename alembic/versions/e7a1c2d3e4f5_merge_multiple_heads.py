30a46d416ad3d52fde6e1744ff4371c8b1961d2a
davidguzman1991 <davidguzman.med@gmail.com>
2025-12-27 21:50:19 -0500

    chore(db): merge alembic heads

---
+++ alembic/versions/e7a1c2d3e4f5_merge_multiple_heads.py
@@ -0,0 +1,23 @@
+"""merge multiple heads
+
+Revision ID: e7a1c2d3e4f5
+Revises: c2d8f1a7b4e9, d2f3a4b5c6d7
+Create Date: 2025-12-27 15:20:00.000000
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "e7a1c2d3e4f5"
+down_revision = ("c2d8f1a7b4e9", "d2f3a4b5c6d7")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
